0044a5a8e59e569d11e506e80928f5c9f784f561
athiyahansari <athiyahansari@gmail.com>
2025-07-31 16:57:32 +0530

    base edit on vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/RSVP/",  
   plugins: [
     react(),
     // The code below enables dev tools like taking screenshots of your site
